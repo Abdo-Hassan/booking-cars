f8d64f4f3b1584fd86ab0c0f84e819f61409c59c
abdo <abdoholmes97@gmail.com>
2022-10-01 14:56:48 +0200

    feat: created car card for booking

--- src/components/CarsSection/CarCard.js
+++ src/components/CarsSection/CarCard.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Car1 from '../../assets/cards/car1.png';
+import Person from '../../assets/icons/person.svg';
+import Favorite from '../../assets/icons/favorite.svg';
+import FavoriteDone from '../../assets/icons/favorite-done.svg';
+import CarIcon2 from '../../assets/icons/carIcon2.svg';
 
-const CarCard = ({ title }) => {
+const CarCard = ({ title, country, users, transmission, rent }) => {
+  const [favorite, setFavorite] = useState(false);
   return (
-    <div className='bg-white rounded-lg p4'>
-      <h6 className='text-2xl'>{title}</h6>
+    <div className='bg-white rounded-2xl px-6 py-8'>
+      <div className='flex items-center justify-between'>
+        <h5 className='text-2xl font-bold'>{title}</h5>
+        {favorite ? (
+          <img
+            className='cursor-pointer'
+            src={FavoriteDone}
+            alt='FavoriteDone'
+            onClick={() => setFavorite(false)}
+          />
+        ) : (
+          <img
+            className='cursor-pointer'
+            src={Favorite}
+            alt='Favorite'
+            onClick={() => setFavorite(true)}
+          />
+        )}
+      </div>
+      <h6 className='text-xl text-slate-500 font-normal'>{country}</h6>
+
+      <img className='mx-auto my-7' src={Car1} alt='carBrand' />
+
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center'>
+          <img src={Person} alt='users' className='mr-1' />
+          <span className='text-slate-500 text-xl font-normal mr-4'>
+            {users}
+          </span>
+          <img src={CarIcon2} alt='transmission' className='mr-2' />
+          <span className='text-md text-slate-500 font-normal mr-2'>
+            {transmission}
+          </span>
+        </div>
+
+        <div>
+          <span className='text-xl'>${rent}</span>
+          <span className='text-xl text-slate-500'>/d</span>
+        </div>
+      </div>
     </div>
   );
 };
